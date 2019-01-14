1441bdc8e008f252167580f3e714bbf100dfdc9b
Micha Rijsmus <1759213rijsmus@zuyd.nl>
2019-01-14 13:06:57 +0100

    test

--- Wandelen/Wandelen/LoginPage.xaml.cs
+++ Wandelen/Wandelen/LoginPage.xaml.cs
@@ -1,30 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Android.Widget;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Wandelen
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
 	{
-		public LoginPage ()
+        public bool IsToggled { get; set; }
+
+
+        public LoginPage ()
 		{
 			InitializeComponent ();
 		}
-
-        private void Inloggen_Pressed(object sender, EventArgs e)
-        {
-            //
-        }
 
         private void Inloggen_Clicked(object sender, EventArgs e)
         {
             //
         }
-	}
+
+        private void WachtwoordVergeten_Clicked(object sender, EventArgs e)
+        {
+            //
+        }
+
+        
+
+        private void OnthoudenGebruiker_Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (IsToggled == true)
+            {
+                //string RememberMe = "Uw naam wordt onthouden";
+                Toast.MakeText(Android.App.Application.Context, "Uw naam wordt onthouden!", ToastLength.Long).Show();
+            }
+            else
+            {
+                //string ForgetMe = "U bent vergeten";
+                Toast.MakeText(Android.App.Application.Context, "U bent vergeten", ToastLength.Long).Show();
+            }
+        }
+    }
 }
