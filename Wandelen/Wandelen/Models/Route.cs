4714123f798943d276a7a60648c27b37007ec447
Natasja Pisters <1755234pisters@zuyd.nl>
2019-01-15 21:16:05 +0100

    Methoden klaargezet voor klasse Route. App.xaml styles klaargezet, maar nog geen manier gevonden om deze globaal te linken.

--- Wandelen/Wandelen/Models/Route.cs
+++ Wandelen/Wandelen/Models/Route.cs
@@ -6,5 +6,29 @@ namespace Wandelen.Models
 {
     class Route
     {
+        public Route()
+        { }
+        public void EigenRoutesOphalen()
+        {
+
+        }
+        //public void OpslaanRoute(idRoute: int, naamRoute: string,
+        //                         lengteRoute: double, tijdRoute: Datetime,
+        //                         moeilijkheidsGraad: string, wegbewijzering: bool)
+        //{
+        //}
+
+        //public void WijzigRoute(idRoute: int, naamRoute: string,
+        //            lengteRoute: double, tijdRoute: Datetime,
+        //            moeilijkheidsGraad: string, wegbewijzering: bool)
+        //{
+        //}
+
+        //public void VerwijderRoute(idRoute: int, naamRoute: string,
+        //            lengteRoute: double, tijdRoute: Datetime,
+        //            moeilijkheidsGraad: string, wegbewijzering: bool)
+        //{
+        //}
+
     }
 }
