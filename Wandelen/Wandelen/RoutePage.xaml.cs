9f246bb7cf98150779dab59a703e6a1cc3ad48bc
Natasja Pisters <1755234pisters@zuyd.nl>
2019-01-18 16:17:44 +0100

    Listview aangemaakt met MenuItem

--- Wandelen/Wandelen/RoutePage.xaml.cs
+++ Wandelen/Wandelen/RoutePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,79 +14,50 @@ namespace Wandelen
     //[XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RoutePage : ContentPage
     {
+        public static ObservableCollection<string> items { get; set; }
         public RoutePage()
         {
+            items = new ObservableCollection<string>() { "Route Samenstellen", "Voorgestelde Routen", "Mijn Routes" };
             InitializeComponent();
+        }
+        void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            DisplayAlert("Item Selected", e.SelectedItem.ToString(), "Ok");
+        }
 
-            //var map = new Map(MapSpan.FromCenterAndRadius(new Position(50.947985, 5.979986), Distance.FromMeters(200)))
-            //{
-            //    IsShowingUser = true,
-            //    VerticalOptions = LayoutOptions.FillAndExpand
-            //};
+        void OnRefresh(object sender, EventArgs e)
+        {
+            var list = (ListView)sender;
+            //put your refreshing logic here
+            var itemList = items.Reverse().ToList();
+            items.Clear();
+            foreach (var s in itemList)
+            {
+                items.Add(s);
+            }
+            //make sure to end the refresh state
+            list.IsRefreshing = false;
+        }
 
-            //var position1 = new Position(50.947230, 5.979053);
-            //var position2 = new Position(50.944773, 5.981314);
-            //var position3 = new Position(50.946206, 5.983832);
-            //var position4 = new Position(50.947985, 5.979986);
-            //var position5 = new Position(50.949290, 5.981625);
-            //var position6 = new Position(50.948865, 5.979721);
+        void OnTap(object sender, ItemTappedEventArgs e)
+        {
+            DisplayAlert("Item Tapped", e.Item.ToString(), "Ok");
+        }
 
-            //var pin1 = new Pin
-            //{
-            //    Type = PinType.Place,
-            //    Position = position1,
-            //    Label = "POI1",
-            //    Address = "street adress poi1",
-            //};
+        void OnMore(object sender, EventArgs e)
+        {
+            var item = (MenuItem)sender;
+            DisplayAlert("More Context Action", item.CommandParameter + " more context action", "OK");
+        }
 
-            //var pin2 = new Pin
-            //{
-            //    Type = PinType.Place,
-            //    Position = position1,
-            //    Label = "POI2",
-            //    Address = "street adress poi2",
-            //};
-
-            //var pin3 = new Pin
-            //{
-            //    Type = PinType.Place,
-            //    Position = position1,
-            //    Label = "POI3",
-            //    Address = "street adress poi3",
-            //};
-
-            //var pin4 = new Pin
-            //{
-            //    Type = PinType.Place,
-            //    Position = position1,
-            //    Label = "POI4",
-            //    Address = "street adress poi4",
-            //};
-
-            //var pin5 = new Pin
-            //{
-            //    Type = PinType.Place,
-            //    Position = position1,
-            //    Label = "POI5",
-            //    Address = "street adress poi5",
-            //};
-
-            //var pin6 = new Pin
-            //{
-            //    Type = PinType.Place,
-            //    Position = position1,
-            //    Label = "POI6",
-            //    Address = "street adress poi6",
-            //};
-
-            //map.Pins.Add(pin1);
-            //map.Pins.Add(pin2);
-            //map.Pins.Add(pin3);
-            //map.Pins.Add(pin4);
-            //map.Pins.Add(pin5);
-            //map.Pins.Add(pin6);
-
-            //Content = map;
+        void OnDelete(object sender, EventArgs e)
+        {
+            var item = (MenuItem)sender;
+            items.Remove(item.CommandParameter.ToString());
         }
     }
 }
